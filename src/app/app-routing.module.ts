b35041d171dce390a85f030b04e7504034696f09
Srinivasan <ssrinivasan@PRCINS.NET>
2019-09-14 21:22:25 -0400

    reversing default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
   { path: 'releases', component: ReleaseComponent },
   { path: 'team-calendar', component: TaskCalendarComponent, },
   { path: 'my-calendar', component: TaskCalendarComponent, },
-  { path: '**', redirectTo: 'login/ssrinivasan?isAdmin=admin' }
+  { path: '**', redirectTo: 'team-calendar' }
 ];
 
 @NgModule({
